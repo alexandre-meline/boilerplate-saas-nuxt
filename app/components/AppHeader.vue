0854e9d7162e050840cb5e68aa9cebe636635086
_/\.|\/|_ <104438775+alexandre-meline@users.noreply.github.com>
2024-09-17 17:58:52 +0200

    Errors msg's and logout btn  implemented

--- app/components/AppHeader.vue
+++ app/components/AppHeader.vue
@@ -1,6 +1,8 @@
 <script setup lang="ts">
 import type { NavItem } from '@nuxt/content'
 
+const supabase = useSupabaseClient()
+const user = useSupabaseUser()
 const navigation = inject<Ref<NavItem[]>>('navigation', ref([]))
 
 const links = [{
@@ -13,12 +15,23 @@ const links = [{
   label: 'Blog',
   to: '/blog'
 }]
+
+async function logout() {
+  try {
+    await supabase.auth.signOut()
+    user.value = null
+    navigateTo('/login')
+  } catch (error) {
+    console.error('Error logging out:', error.message)
+  }
+}
 </script>
 
 <template>
   <UHeader :links="links">
     <template #logo>
-      Nuxt UI Pro <UBadge
+      Ecole 2600 - Saas Boilerplate
+      <UBadge
         label="SaaS"
         variant="subtle"
         class="mb-0.5"
@@ -26,19 +39,28 @@ const links = [{
     </template>
 
     <template #right>
-      <UButton
-        label="Sign in"
-        color="gray"
-        to="/login"
-      />
-      <UButton
-        label="Sign up"
-        icon="i-heroicons-arrow-right-20-solid"
-        trailing
-        color="black"
-        to="/signup"
-        class="hidden lg:flex"
-      />
+      <template v-if="user">
+        <UButton
+          label="Logout"
+          color="red"
+          @click="logout"
+        />
+      </template>
+      <template v-else>
+        <UButton
+          label="Sign in"
+          color="gray"
+          to="/login"
+        />
+        <UButton
+          label="Sign up"
+          icon="i-heroicons-arrow-right-20-solid"
+          trailing
+          color="black"
+          to="/signup"
+          class="hidden lg:flex"
+        />
+      </template>
     </template>
 
     <template #panel>
